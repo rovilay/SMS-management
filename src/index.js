66fb32aee744533a4be3ded958b10caa4070a880
rovilay <rovilayjnr01@gmail.com>
2019-04-17 01:14:28 +0100

    ch-setup-server
    * setup server
    * setup eslint
    * setup es6 support
    * setup routes
    * setup errorhandler

---
+++ src/index.js
@@ -0,0 +1,22 @@
+/* eslint no-console: 0 */
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import routes from './routes';
+
+dotenv.config();
+
+const { PORT = 5555 } = process.env;
+
+const app = express();
+
+app.use(cors());
+// init routes
+routes(app);
+
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
+});
+
+export default app;
