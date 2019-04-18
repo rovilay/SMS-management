10882479f7db6ee66f7023510b7beb81f6b55dab
rovilay <rovilayjnr01@gmail.com>
2019-04-18 04:46:53 +0100

    ch-setup-server (#1)
    * setup server
    * setup eslint
    * setup es6 support
    * setup routes
    * setup errorhandler

---
+++ src/helpers/defaults.js
@@ -0,0 +1,5 @@
+export const apiPrefix = '/api/v1';
+export const smsApiPrefix = '/sms';
+export const appWelcomeMsg = 'Welcome to sms management app';
+export const nonExistingRouteMsg = 'route do not exist!';
+export const genericErrorMessage = 'something went wrong';
